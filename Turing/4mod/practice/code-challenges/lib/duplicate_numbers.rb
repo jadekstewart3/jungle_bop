e3cf9e54b5680aafe3542f660e2aa0a8b4315e1b
Jade Stewart <jadekstewart3@gmail.com>
2023-05-31 10:21:33 -0600

    duplicates in an array two ways

---
+++ Turing/4mod/practice/code-challenges/lib/duplicate_numbers.rb
@@ -0,0 +1,16 @@
+# def duplicates(arr)
+#   if arr.uniq.size == arr.size
+#     false
+#   else
+#     true
+#   end
+# end
+
+##return the intger
+
+def duplicates(nums)
+  grouped = nums.group_by { |num| num }
+  grouped.find do |k, v|
+    k if v.size.even?
+  end[0]
+end
