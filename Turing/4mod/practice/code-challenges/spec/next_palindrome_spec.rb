d8906a426a7d62481b6ca368f21952e74d52c708
Jade Stewart <jadekstewart3@gmail.com>
2023-06-01 16:59:02 -0600

    little test for reduce

--- Turing/4mod/practice/code-challenges/spec/next_palindrome_spec.rb
+++ Turing/4mod/practice/code-challenges/spec/next_palindrome_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe 'next palindrome' do
     expect(find_next_palindrome(100)).to eq(101)
     expect(find_next_palindrome(12320)).to eq(12321)
   end
+
+  # it "helps me understand reduce" do 
+  #   expect(wtr("string")).to eq("gnirts")
+  # end
 end
