605a4ba043fc717d9102731b210b861da297a87d
Jade Stewart <jadekstewart3@gmail.com>
2023-06-01 16:38:14 -0600

    palindrome solution without using reverse

--- Turing/4mod/practice/code-challenges/lib/next_palindrome.rb
+++ Turing/4mod/practice/code-challenges/lib/next_palindrome.rb
@@ -6,8 +6,16 @@
 #if it is equal return it if not add one and repeat
 # do I need to count zeros in front
 
-def  find_next_palindrome(int)
-  unless int == int.to_s.reverse.to_i
+# def  find_next_palindrome(int)
+#   unless int == int.to_s.reverse.to_i
+#     int + 1
+#   end
+# end
+
+#solution without using reverse
+
+def find_next_palindrome(int)
+  unless int == int.to_s.chars.reduce { |x, y| y + x }
     int + 1
   end
 end
