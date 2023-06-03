6ce907c854b77cd862f316d44be332bd439fd9cd
Jade Stewart <jadekstewart3@gmail.com>
2023-06-03 16:01:18 -0600

    continue working on snail problem

--- Turing/4mod/practice/code-challenges/lib/snail.rb
+++ Turing/4mod/practice/code-challenges/lib/snail.rb
@@ -7,26 +7,8 @@
 ##are the arrays always going to be the same size?
 #.each with index
 
-def snail(arr_matrix)
-  arr_matrix.each_with_index do |arr|
-    require 'pry'; binding.pry
-    snail_arr = []
-    if arr[0] 
-      snail_arr << arr
-    elsif arr.each_with_index do |num|
-      if num[2] 
-        snail_arr << num
-      elsif num == 8 
-        snail_arr << num
-      elsif num == 7 
-        snail_arr << num
-      elsif num == 4
-        snail_arr << num
-      else
-        snail_arr << num
-      end
-    end
-  end
-  snail_arr
-end
+def snail(array)
+    arr = []
+   array.to_a.empty? ? (return arr.flatten) : arr << array.shift
+  snail(array.transpose.reverse)
 end
