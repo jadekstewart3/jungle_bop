709741a67760c198eff1c1d02d0679c58f71c1ea
Jade Stewart <jadekstewart3@gmail.com>
2023-07-21 11:26:39 -0600

    printer error solutions with regex exploration

---
+++ Turing/4mod/practice/code-challenges/lib/printer_error.rb
@@ -0,0 +1,19 @@
+# first attempt
+#this is faster?
+
+def printer_error(s)
+  errors = []
+  non_error_letters = ("a".."m")
+  colors_used = s.chars
+  colors_used.each do |letter|
+    errors << letter if !non_error_letters.include?(letter)
+  end
+  "#{errors.count}/#{colors_used.count}"
+end
+
+#chat jeeps suggestions
+
+def printer_error(s)
+  error_count = s.scan(/[^a-m]/).count #scan and regex powerful af
+  "#{error_count}/#{s.length}"
+end
