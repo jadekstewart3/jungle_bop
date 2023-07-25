701b68a9ec33d9879f4537f88014ef9e5e72c4ab
Jade Stewart <jadekstewart3@gmail.com>
2023-07-25 10:06:37 -0600

    first attempt and refactored solution

---
+++ Turing/4mod/practice/code-challenges/lib/remove_first_and_last.rb
@@ -0,0 +1,14 @@
+#first attempt
+# def remove_char(s)
+#   array = s.chars
+#   s = array.delete_at(0)
+#   s = array.delete_at(-1)
+#   s = array.join
+#   return s
+# end
+
+#refactor
+
+def remove_char(s)
+  s.chars[1..-2].join
+end
