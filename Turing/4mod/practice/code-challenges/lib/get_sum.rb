ee36211c0d16d97f1c990c59b39731551c1b339e
Jade Stewart <jadekstewart3@gmail.com>
2023-08-29 13:22:32 -0600

    add notes on parallel assignment

--- Turing/4mod/practice/code-challenges/lib/get_sum.rb
+++ Turing/4mod/practice/code-challenges/lib/get_sum.rb
@@ -1,5 +1,8 @@
 def get_sum(a, b)
   #parallel assignment
+  # a or b = b, its a if a is greater than b
   a, b = b, a if a > b
   (a..b).sum
 end
+
+#could also [a,b].sort then reduce also using parallel assignment
