d7a2574f796b43afbe6e7ff4aa45965ee0d8cb6f
Jade Stewart <jadekstewart3@gmail.com>
2023-08-31 12:10:07 -0600

    refactored solution

--- Turing/4mod/practice/code-challenges/lib/square_digits.rb
+++ Turing/4mod/practice/code-challenges/lib/square_digits.rb
@@ -1,6 +1,11 @@
 
+# def square_digits(num)
+#   num.to_s.chars.map do  |int|
+#     int.to_i * int.to_i
+#   end.join.to_i
+# end
+
+#refactored but code isnt as DRY as I would like
 def square_digits(num)
-  num.to_s.chars.map do  |int|
-    int.to_i * int.to_i
-  end.join.to_i
+  num.to_s.chars.map { |int| int.to_i * int.to_i }.join.to_i
 end
