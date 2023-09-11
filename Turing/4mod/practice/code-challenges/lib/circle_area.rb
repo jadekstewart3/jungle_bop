a42aa4287682d1e71586e28f5c40211050186b97
Jade Stewart <jadekstewart3@gmail.com>
2023-09-11 14:34:56 -0600

    refactor

--- Turing/4mod/practice/code-challenges/lib/circle_area.rb
+++ Turing/4mod/practice/code-challenges/lib/circle_area.rb
@@ -1,13 +1,21 @@
 # first attempt
 
+# def circle_area(r)
+#  pi = 3.14159265359
+#   if r.class == String
+#     false
+#   elsif r.negative? || r == 0
+#     false
+#   else
+#     area = pi * r**2
+#     area.round(2)
+#   end
+# end
+
 def circle_area(r)
  pi = 3.14159265359
-  if r.class == String
-    false
-  elsif r.negative? || r == 0
-    false
-  else
-    area = pi * r**2
-    area.round(2)
-  end
+ return false if r.is_a?(String) || r <= 0
+  
+  area = pi * r**2
+  area.round(2)
 end
