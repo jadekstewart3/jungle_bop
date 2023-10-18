678cf22566af372b437c1a253fb709253b333a02
Jade Stewart <jadekstewart3@gmail.com>
2023-10-18 08:09:48 -0600

    add alternate solution with notes on why it works

--- Turing/4mod/practice/code-challenges/lib/break_camel_case.rb
+++ Turing/4mod/practice/code-challenges/lib/break_camel_case.rb
@@ -1,10 +1,17 @@
+# def solution(string)
+#   modified_string = ""
+#   string.each_char.with_index do |char, i|
+#     if char == char.upcase
+#       modified_string << " "
+#     end
+#     modified_string << char
+#   end
+#   modified_string
+# end
+
+#other solutions
 def solution(string)
-  modified_string = ""
-  string.each_char.with_index do |char, i|
-    if char == char.upcase
-      modified_string << " "
-    end
-    modified_string << char
-  end
-  modified_string
+  string.gsub /([A-Z])/, ' \1'
 end
+#when an uppercase letter is found it captures it, 
+# in replacement string it inserts a space
